b1425fdfcb569ff7c9381ff7d192cc0fd49965bd
jonathan-nasser <virotico@gmail.com>
2024-05-29 11:26:51 -0300

    novo conteudo no js

--- arquivo.js
+++ arquivo.js
@@ -1,14 +1,48 @@
 console.log("Jonathan Nasser")
 
-const n = 32/2
-const soma = 2+2
+const n = 32 / 2
+const soma = 2 + 2
 
-console.log (n)
+console.log(n)
 
-console.log (soma)
+console.log(soma)
 
-const = true
+console.log(`a existência do hb2 é 
+verdadeira` )
 
-console.log(`a existência do hb2 é ${true}` )
 
+const jonathan = {
+    nome: "Jonathan",
+    idade: 42,
+    endereco: {
+        logradouro: "João Alves de Melo",
+        numero: 81,
+        cidade: "Natal",
+        estado: "RN",
+        pais: "Brasil"
+    }
+}
 
+console.log(jonathan)
+
+console.log(jonathan.endereco)
+
+console.log(jonathan.endereco.logradouro)
+
+//alterando valor
+
+const usuario = { //o const poderá ser usado, pois será alterado só item do objeto. Caso o contrário seria LET
+    nome: "Jonathan",
+    idade: 42,
+    endereco: {
+        logradouro: "João Alves de Melo",
+        numero: 81,
+        cidade: "Natal",
+        estado: "RN",
+        pais: "Brasil"
+    }
+}
+
+usuario.endereco.numero = "81a"
+
+console.log(usuario)
